e4e60b8426251958f40a4a32fce22ab17bff3afb
Ajay_00 <ajaylohith855@gmail.com>
2026-01-04 17:31:41 +0530

    Added Rate Limiting Filter

--- src/main/java/com/ajay/urlshortnerapp/rateLimit/RateLimitFilter.java
+++ src/main/java/com/ajay/urlshortnerapp/rateLimit/RateLimitFilter.java
@@ -1,4 +1,54 @@
 package com.ajay.urlshortnerapp.rateLimit;
 
-public class RateLimitFilter {
+import com.ajay.urlshortnerapp.rateLimit.RateLimitService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class RateLimitFilter extends OncePerRequestFilter {
+
+    private final RateLimitService rateLimitService;
+
+    @Value("${rate-limit.requests}")
+    private int maxRequests;
+
+    @Value("${rate-limit.window-seconds}")
+    private int windowSeconds;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String clientIp = request.getRemoteAddr();
+        String key = "rate:" + clientIp;
+
+        try {
+            boolean allowed = rateLimitService.isAllowed(
+                    key, maxRequests, windowSeconds
+            );
+
+            if (!allowed) {
+                response.setStatus(429);
+//                System.out.println("Too Many Requests");
+                response.getWriter().write("Too Many Requests");
+                return;
+            }
+        } catch (Exception e) {
+            // FAIL OPEN: Redis down should not block traffic
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
